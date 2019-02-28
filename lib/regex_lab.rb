6a0a2c83ad6108dd41aa638c358689a0c11c8b08
Brian Simonton <bsimonton78@gmail.com>
2019-02-28 15:29:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -22,13 +22,12 @@ def words_five_letters_long(text)
 text.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
- 
 
+
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  
+ end
 def valid_phone_number?(phone)
-if phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
-    true
-  else
-    false
-  end
+
 end
